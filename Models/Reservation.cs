a7d5073c0e22c5b8f5799291e021920037845889
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-04-15 19:30:22 +0200

    Fixed files

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -1,6 +1,29 @@
-﻿namespace ApplicationAPBD_6.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationAPBD_6.Models
 {
     public class Reservation
     {
+        public int Id { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int RoomId { get; set; }
+
+        [Required] 
+        public string OrganizerName { get; set; } = string.Empty;
+               
+        [Required]
+        public string Topic { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateOnly Date { get; set; }
+
+        [DataType(DataType.Time)]
+        public TimeOnly StartTime { get; set; }
+
+        [DataType(DataType.Time)]
+        public TimeOnly EndTime { get; set; }
+
+        public string Status { get; set; } = string.Empty;
     }
 }
